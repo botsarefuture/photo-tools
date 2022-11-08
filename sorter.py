1016a01ddce1f018129d6b2a62549a2786295307
botsarefuture <vuoreol@pm.me>
2022-11-08 09:43:12 +0200

    Changes

--- sorter.py
+++ sorter.py
@@ -19,6 +19,13 @@ print("Video formats ready")
 print("Checking paths")
 if not os.path.exists(DIRS_PATH):
 	os.mkdir(DIRS_PATH)
+
+def date(text):
+    year = text[0:4]
+    month = text[5:7]
+    day = text[8:10]
+    return day, month, year
+
 def get_photos(path):
     for root, dirs, files in os.walk(path):
         for f in files:
@@ -32,32 +39,57 @@ def get_photos(path):
 fail_count = 0
 success_count = 0
 
+def format_date(day, month, year):
+    date_path = f"{day}.{month}.{year}"
+
+def format_suunta(y, x, suunta_path):
+    if y == 720:
+        suunta_path += "/720p"
+    elif y == 1080 and x == 1920 or x == 1080 and x == 1920:
+        suunta_path += "/1080p"
+    elif y == 1080 and x == 2048 or x == 1080 and y == 2048:
+        suunta_path += "/2k"
+    elif y == 2160 and x == 3840 or x == 2160 and y == 3840:
+        suunta_path += "/4k"
+    else:
+        suunta_path = f"/{y}"
+    return suunta_path
 for img in IMAGES:
     with open(img, "rb") as file:
         tags = exifread.process_file(file, details=False, stop_tag="DateTimeOriginal")
         try:
             if "EXIF DateTimeOriginal" in tags:
-                date_path = str(tags["EXIF DateTimeOriginal"])[:10].replace(":", ".")
+                day, month, year = date(str(tags["EXIF DateTimeOriginal"])[:10].replace(":", "."))
+                format_date(day, month, year)
                 success_count += 1
             if not "EXIF DateTimeOriginal" in tags and "EXIF ModifyDate" in tags:
-                date_path = str(tags["EXIF ModifyDate"])[:10].replace(":", ".")
+                day, month, year = date(str(tags["EXIF ModifyDate"])[:10].replace(":", "."))
+                format_date(day, month, year)
                 success_count += 1
         except:
             print(str(img) + " does not have EXIF tags.")
             fail_count += 1
             date_path = "0000"
-            y = str(tags["YResolution"])
-            x = str(tags["XResolution"])
-            if y < x:
-                if not os.path.exists(DIRS_PATH + date_path + "vaaka"):
-                    os.mkdir(DIRS_PATH + date_path + "vaaka")
-            elif x < y:
-                if not os.path.exists(DIRS_PATH + date_path + "pysty"):
-                    os.mkdir(DIRS_PATH + date_path + "pysty")
-            elif x == y:
-                if not os.path.exists(DIRS_PATH + date_path + "nelio"):
-                    os.mkdir(DIRS_PATH + date_path + "nelio")
-	shutil.move(img, dirs_path + date_path + "\\" + img[-12:])
+        y = str(tags["YResolution"])
+        x = str(tags["XResolution"])
+        if y < x:
+            suunta_path = "vaaka"
+            if not os.path.exists(DIRS_PATH + date_path + suunta_path):
+                os.mkdir(DIRS_PATH + date_path + suunta_path)
+            suunta_path = format_suunta(y, x, suunta_path)
+        elif x < y:
+            suunta_path = "pysty"
+            if not os.path.exists(DIRS_PATH + date_path + "pysty" ):
+                os.mkdir(DIRS_PATH + date_path + "pysty")
+            suunta_path = format_suunta(y, x, suunta_path)
+            
+        elif x == y:
+            suunta_path = "nelio"
+            if not os.path.exists(DIRS_PATH + date_path + "nelio"):
+                os.mkdir(DIRS_PATH + date_path + "nelio")
+            suunta_path = format_suunta(y, x, suunta_path)
+
+    shutil.move(img, DIRS_PATH + date_path + "/"  + suunta_path + "/" + img[-12:])
 
 print("Sorted " + str(success_count) + " files.") # Images properly sorted by date taken
 print("Failed to sort " + str(fail_count) + " files.") # Images sent to 0000
