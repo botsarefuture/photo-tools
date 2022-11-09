020438488b29278c0ef4e706161ab6c1a71bc66e
botsarefuture <vuoreol@pm.me>
2022-11-09 12:27:34 +0200

    yEAH

--- sorter.py
+++ sorter.py
@@ -3,7 +3,7 @@ import os.path
 import shutil
 import exifread
 from funcs import calculate_aspect, suunta, format_date, date, suunta_maaritus
-from config import VIDEO1_FORMATS, VIDEO_FORMATS, PHOTO_FORMATS, DIRS_PATH, IMAGES, VIDEOS, SUCCESS_COUNT, FAIL_COUNT
+from config import VIDEO1_FORMATS, VIDEO_FORMATS, PHOTO_FORMATS, DIRS_PATH, IMAGES, VIDEOS, SUCCESS_COUNT, FAIL_COUNT, DATE_FOR
 
 print("Cleaning formats lists")
 for format in VIDEO_FORMATS:
@@ -18,15 +18,16 @@ for img in IMAGES:
     with open(img, "rb") as file:
         tags = exifread.process_file(file, details=False, stop_tag="DateTimeOriginal")
         try:
-            DATE_FOR = ["DateTimeOriginal", "ModifyDate"]
-            susse = False
+            
+            success = False
             for item in DATE_FOR:
-                if not susse == True:
+                if not success == True:
                     if f"EXIF {item}" in tags:
                         day, month, year = date(str(tags[f"EXIF {item}"])[:10].replace(":", "."))
                         date_path = format_date(day, month, year)
                         SUCCESS_COUNT += 1
-                        susse = True
+                        success = True
+
         except:
             print(str(img) + " does not have EXIF tags.")
             FAIL_COUNT += 1
